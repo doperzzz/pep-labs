79756c246310159a3bea9519b66930eb0b1bbc65
doperzzz <preston.zaya@gmail.com>
2023-01-07 15:51:32 +0000

    Saved progress on HardcodedLogin 2023-01-07 15:51:32 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -28,6 +28,14 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
+        
+        String str;
+      for(int i = 0; i < nums.length; i++){
+           //do something to str here
+      }
+      return str;
+        
+        
         return "";
     }
 }
