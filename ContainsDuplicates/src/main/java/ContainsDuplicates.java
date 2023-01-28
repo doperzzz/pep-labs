239609447147c1ef96cc18a33576b986662dce19
doperzzz <preston.zaya@gmail.com>
2023-01-28 22:07:23 +0000

    Saved progress on ContainsDuplicates 2023-01-28 22:07:23 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,16 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Set<Integer> numbers = new HashSet<Integer>();
+        
+        for( int i = 0; i < nums.length; i++){
+            numbers.add(i);
+            if ( numbers.contains(nums[i])){
+                return true;
+            }
+    
+        }
         return false;
     }
 }
+
