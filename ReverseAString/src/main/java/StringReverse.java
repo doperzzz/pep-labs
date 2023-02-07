31b047364d9bdf5845ba16ab70f7eeef05d80a50
doperzzz <preston.zaya@gmail.com>
2023-02-07 21:41:20 +0000

    Saved progress on ReverseAString 2023-02-07 21:41:20 +0000

--- ReverseAString/src/main/java/StringReverse.java
+++ ReverseAString/src/main/java/StringReverse.java
@@ -13,6 +13,8 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+       StringBuilder backwards = new StringBuilder(str);
+        backwards.reverse();
+        return backwards.toString();
     }
 }
